2eb7a2e1b81936eb8dc57ffd9e314abee6494fa3
skarticsinha <110276422+skarticsinha@users.noreply.github.com>
2022-11-01 13:10:25 +0500

    added Resposiveness
    Header and footer is made resopnsive across all the pages

--- js/header.js
+++ js/header.js
@@ -8,3 +8,11 @@ window.addEventListener("scroll", () => {
     }
 });
 
+const mobile_nav = document.querySelector (".mobile_navbar_btn");
+const nav_header = document.querySelector (".mobile_header");
+
+const toggleNavbar = () => {
+   nav_header.classList.toggle("menu_active");
+}
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
